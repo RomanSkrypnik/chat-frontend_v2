db962a1a9699fd204dcac1c2940ad7d4aca1fbfe
unknown <rorchenko@gmail.com>
2022-02-16 23:44:58 +0200

    govno tam nado dodelat

--- src/components/UI/buttons/AvatarButton.jsx
+++ src/components/UI/buttons/AvatarButton.jsx
@@ -5,6 +5,7 @@ const AvatarButton = ({status, large, disabled = false}) => {
 
     return (
         <button className={cn("avatar", large && 'avatar_large')} disabled={disabled}>
+            <div className="avatar__overlay" />
             <span className={cn("avatar__circle", status ?? 'd-none')} />
             {/*<img src={process.env.PUBLIC_URL + '/images/sidebar/1.jpg'} alt="Avatar" className="avatar__image"/>*/}
         </button>
--- src/components/partials/ContactInfo.jsx
+++ src/components/partials/ContactInfo.jsx
@@ -6,7 +6,7 @@ import cn from "classnames";
 import {useSelector} from "react-redux";
 import ContactButton from "../UI/buttons/ContactButton";
 
-const ContactInfo = ({user, alignToCenter = false, onStatusChange, onClose, buttons}) => {
+const ContactInfo = ({user, alignToCenter = false, onStatusChange, onClose, buttons, disabledAvatar = true}) => {
     const auth = useSelector(state => state.auth);
 
     return (
@@ -16,7 +16,7 @@ const ContactInfo = ({user, alignToCenter = false, onStatusChange, onClose, butt
             </div>
             <div className="contact-info__content d-flex flex-column align-items-center mt-4">
                 <h2 className="last-text last-text_contact text-purple mb-3">Contact Info</h2>
-                <AvatarButton large disabled/>
+                <AvatarButton large disabled={disabledAvatar}/>
                 <div className="last-text last-text_contact-alt text-purple mt-3">{user.name}</div>
                 <div className="regular-text my-2">Sr. Visual Designer</div>
                 <StatusSelect selectedStatus={user.status}
