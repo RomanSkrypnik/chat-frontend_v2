643047a3b870f8e5bd8d2baa3e092d769facb0bf
unknown <rorchenko@gmail.com>
2022-02-20 21:12:42 +0200

    avatar animation

--- src/components/UI/buttons/AvatarButton.jsx
+++ src/components/UI/buttons/AvatarButton.jsx
@@ -1,35 +1,39 @@
 import React from 'react';
 import cn from 'classnames';
 import {API_URL} from "../../../http";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {uploadAvatar} from "../../../store/slices/auth";
 
-const AvatarButton = ({user, large, disabled = false}) => {
+const AvatarButton = ({user, large}) => {
     const dispatch = useDispatch();
+
+    const currentUser = useSelector(state => state.auth.user);
+    const isCurrentUser = currentUser.hash === user.hash;
 
     const onAvatarUpload = (e) => {
         const fd = new FormData();
         fd.append('avatar', e.target.files[0]);
         dispatch(uploadAvatar(fd));
-    }
+    };
 
     return (
-        <button className={cn("avatar p-0", large && 'avatar_large')} disabled={disabled}>
-            <span className="avatar__wrapper position-relative overflow-hidden h-100">
-                {user.pictureUrl && <img src={API_URL + '/img/' + user.pictureUrl} alt="Avatar" className="avatar__image h-100"/>}
+        <button className={cn("avatar p-0", large && 'avatar_large')}>
+            <span className="avatar__wrapper">
+                {user.pictureUrl &&
+                <img src={API_URL + '/img/' + user.pictureUrl} alt="Avatar" className="avatar__image"/>}
             </span>
             <span className={cn("avatar__circle", user?.status?.className ?? 'd-none')}/>
-            <label htmlFor="avatar" className="w-100 h-100">
-                <input
-                    type="file"
-                    name="avatar"
-                    id="avatar"
-                    className="avatar_input"
-                    onChange={onAvatarUpload}
-                    size="60"
-                    disabled={disabled}
-                />
-            </label>
+            {
+                large && isCurrentUser &&
+                <label htmlFor="avatar" className="w-100 h-100">
+                    <input
+                        type="file"
+                        name="avatar"
+                        id="avatar"
+                        className="avatar_input"
+                        onChange={onAvatarUpload}
+                    />
+                </label>}
         </button>
     );
 };
