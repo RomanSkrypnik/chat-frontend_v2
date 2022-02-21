1556c0b502cfcfa45aee861c66dbb43658c00f24
rorchenko <rorchenko@gmail.com>
2022-02-21 20:01:29 +0200

    4eto

--- src/components/partials/PrivacySettingsForm.jsx
+++ src/components/partials/PrivacySettingsForm.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import {Controller, useForm} from "react-hook-form";
+import TextInput from "../inputs/TextInput";
 
 const PrivacySettingsForm = () => {
-    return (
-        <div>
 
-        </div>
+    const {handleSubmit, control} = useForm();
+
+    const onSubmit = () => {
+        console.log('on submit');
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    name="email"
+                    render={({field: {onChange, value}}) => (
+                        <TextInput onChange={onChange} value={value} placeholder="email"/>
+                    )}
+                />
+                <Controller
+                    name="name"
+                    render={({field: {onChange, value}}) => (
+                        <TextInput onChange={onChange} value={value} placeholder="email"/>
+                    )}
+                />
+                <Controller
+                    name="password"
+                    render={({field: {onChange, value}}) => (
+                        <TextInput onChange={onChange} value={value} placeholder="email"/>
+                    )}
+                />
+                <Controller
+                    name="passwordConfirmation"
+                    render={({field: {onChange, value}}) => (
+                        <TextInput onChange={onChange} value={value} placeholder="email"/>
+                    )}
+                />
+            </form>
+        </>
     );
 };
 
