efc7dded0d2a30d0702e186f685b25766fd67cef
rorchenko <rorchenko@gmail.com>
2022-02-23 18:22:51 +0200

    contact info fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -77,7 +77,7 @@ const Home = () => {
                 {
                     contactInfo &&
                     <ContactInfo onClose={handleContactInfo}>
-                        <UserInfo user={friend}/>
+                        {<UserInfo user={friend.friend}/>}
                     </ContactInfo>
                 }
             </div>
