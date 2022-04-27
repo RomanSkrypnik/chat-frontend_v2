d2df27be46f57e7c0762076e9c69d1c1754fde43
unknown <rorchenko@gmail.com>
2022-04-27 19:15:40 +0300

    dropdown

--- src/components/UI/Dropdown.jsx
+++ src/components/UI/Dropdown.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Overlay from "./Overlay";
-import cn from "classnames";
 
-const Dropdown = ({items, onClose, className = ''}) => {
+const Dropdown = ({items, onClose}) => {
+
+    const ref = useRef(null);
+
+    const [top, setTop] = useState(-1000);
+    const [left, setLeft] = useState(-1000);
+
+    useEffect(() => {
+        if (ref) {
+            const parent = ref.current.parentNode;
+
+            const rect = parent.getBoundingClientRect();
+
+            setTop(rect.bottom + 10);
+            setLeft(rect.left - 100);
+        }
+    }, [ref]);
 
     return (
         <>
-            <div className={cn("dropdown", className)}>
+            <div className="dropdown" style={{top, left}} ref={ref}>
                 {items.map((item, index) => <button className="dropdown__item regular-text text-white" onClick={item.onClick} key={index}>{item.text}</button>)}
             </div>
             <Overlay onClick={onClose}/>
